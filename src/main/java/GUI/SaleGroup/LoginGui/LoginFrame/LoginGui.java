be8e4d3d2d30896adc9a8f0134ee4118eacaebda
Zasuz <102852013+Zasuz@users.noreply.github.com>
2022-05-15 00:14:56 +0700

    UpdateGui

--- src/main/java/GUI/SaleGroup/LoginGui/LoginFrame/LoginGui.java
+++ src/main/java/GUI/SaleGroup/LoginGui/LoginFrame/LoginGui.java
@@ -203,7 +203,7 @@ public class LoginGui extends javax.swing.JFrame {
 
         jLabel7.setFont(new java.awt.Font("Segoe UI", 2, 14)); // NOI18N
         jLabel7.setForeground(new java.awt.Color(102, 102, 102));
-        jLabel7.setText("Duy, Phúc, Lâm, Tuấn, Đạt");
+        jLabel7.setText("Phúc, Lâm, Phú , Duy ,Khôi");
 
         jLabel8.setBackground(new java.awt.Color(102, 102, 102));
         jLabel8.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
